a8ebe4ced185b96de9a21fd47c7fab663cf827f4
DavMateo <davidcarrenomc@gmail.com>
2024-09-16 20:45:05 -0500

    feat: :sparkles: Finalización del encabezado
    Se finaliza el diseño del encabezadp y se empieza a migrar ese diseño a
    los webcomponents necesarios. Se agregan estilos nuevos así como
    animaciones y corrección de errores visuales.

--- app/componentesGlobales/headerComponente.js
+++ app/componentesGlobales/headerComponente.js
@@ -6,6 +6,7 @@ export class EncabezadoComponente extends HTMLElement {
 
     render() {
         const cuerpoWebsite = document.getElementById("webPrincipal");
+        const SVG_XMLNS = "http://www.w3.org/2000/svg";
 
         // Creando los elementos HTML necesarios
         let headerEncabezado = document.createElement("header");
@@ -22,6 +23,30 @@ export class EncabezadoComponente extends HTMLElement {
         let liNavContainerBtnLink4 = document.createElement("li");
         let liNavContainerBtnLink5 = document.createElement("li");
 
+        let divContentInfo = document.createElement("div");
+        let divInfoContainer = document.createElement("div");
+        let divPresentacion = document.createElement("div");
+        let h2TituloInfo = document.createElement("h2");
+        let pTextoInfo = document.createElement("p");
+        let divInfoContBtns = document.createElement("div");
+        let buttonVisita = document.createElement("button");
+        let spanBtnVistaTexto = document.createElement("span");
+        let spanBtnVisitaIcon = document.createElement("span");
+        let svgIconContainer = document.createElementNS(SVG_XMLNS, "svg");
+        let circleSvgIcon1 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon2 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon3 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon4 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon5 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon6 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon7 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon8 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon9 = document.createElementNS(SVG_XMLNS, "circle");
+        let circleSvgIcon10 = document.createElementNS(SVG_XMLNS, "circle");
+        let buttonVermas = document.createElement("button");
+        let divImgMuestra = document.createElement("div");
+        let imgMuestra = document.createElement("img");
+
 
         // Creando los nodos de texto correspondientes
         let textTituloEncabezado = document.createTextNode("Arte CampusLands");
@@ -30,6 +55,11 @@ export class EncabezadoComponente extends HTMLElement {
         let textNavContainerBtnLink3 = document.createTextNode("Eventos");
         let textNavContainerBtnLink4 = document.createTextNode("Autores");
         let textNavContainerBtnLink5 = document.createTextNode("Contáctanos");
+
+        let textTituloInfoSection = document.createTextNode("¡Bienvenido a la galería de arte CampusArt!");
+        let textInfoContPresentacion = document.createTextNode("Descubre impresionantes obras de arte de siglos de expresividad, creatividad y pensamiento humano. Nuestra gran colección de pinturas, se ajusta a todo tipo de gustos, ¡De seguro encontrarás la obra perfecta!");
+        let textBtnVisita = document.createTextNode("Planea tu visita");
+        let textBtnVermas = document.createTextNode("Ver más");
 
 
         // Agregándole los atributos a cada etiqueta
@@ -50,6 +80,26 @@ export class EncabezadoComponente extends HTMLElement {
         liNavContainerBtnLink3.setAttribute("class", "navbarContainer__btnLink");
         liNavContainerBtnLink4.setAttribute("class", "navbarContainer__btnLink");
         liNavContainerBtnLink5.setAttribute("class", "navbarContainer__btnLink");
+
+        divContentInfo.setAttribute("class", "contentInfo");
+        divContentInfo.setAttribute("id", "contentInfo");
+        divInfoContainer.setAttribute("class", "infoContainer");
+        divPresentacion.setAttribute("class", "infoContainer__presentacion");
+        h2TituloInfo.setAttribute("class", "infoContPresentacion__titulo");
+        pTextoInfo.setAttribute("class", "infoContPresentacion__texto");
+        divInfoContBtns.setAttribute("class", "infoContainer__btnVisita");
+        buttonVisita.setAttribute("class", "infoContainer__btnVisita");
+        spanBtnVistaTexto.setAttribute("class", "btnVisita__texto");
+        spanBtnVisitaIcon.setAttribute("class", "btnVisita__iconContainer");
+        svgIconContainer.setAttributeNS(null, "width", "16");
+        svgIconContainer.setAttributeNS(null, "height", "19");
+        svgIconContainer.setAttributeNS(null, "viewBox", "0 0 16 19");
+        svgIconContainer.setAttributeNS(null, "fill", "nones");
+        svgIconContainer.setAttributeNS(null, "class", "btnVisita__svgIcon");
+        circleSvgIcon1.setAttributeNS(null, "cx", "1.61321");
+        circleSvgIcon1.setAttributeNS(null, "cy", "1.61321");
+        circleSvgIcon1.setAttribute(null, "r", "1.5");
+        circleSvgIcon1.setAttributeNS(null, "fill", "black");
 
 
         // Insertando los nodos de texto a la etiquetas correspondientes
